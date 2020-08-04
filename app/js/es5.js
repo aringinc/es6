ac19854a7a059a63601b48262b4419bcafe0fc21
Shumylo Volodymyr <aringinc@gmail.com>
2020-08-04 13:19:32 +0300

    added plugins

--- app/js/es5.js
+++ app/js/es5.js
@@ -1,9 +1,6 @@
 "use strict";
 
-console.log('hello');
-console.log(a);
-var a = 11;
-var arr = [1, 2, 3].map(function (n) {
-  return n + 1;
-});
-console.log(arr);
+var map = new Map();
+map.set(Symbol["for"]('test'), '1');
+console.log(map);
+console.log(map.get(Symbol["for"]('test')));
--- app/js/es6.js
+++ app/js/es6.js
@@ -1,6 +1,8 @@
-console.log('hello');
+let map = new Map()
 
-console.log(a);
-const a = 11;
-const arr = [1, 2, 3].map((n) => n + 1);
-console.log(arr);
+map.set(Symbol.for('test'), '1')
+
+console.log(map);
+
+
+console.log(map.get(Symbol.for('test')));
